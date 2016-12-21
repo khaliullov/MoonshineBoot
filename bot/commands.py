bc78c33b260877db87947080daa9bf5fc5a1594f
Leandr Khaliullov <lh@drivecred.com>
2016-12-21 12:06:53 +0300

    added help message for `help` or `start` command

--- bot/commands.py
+++ bot/commands.py
@@ -15,7 +15,17 @@ class CommandsProcessor(object):
         self.logger = logging.getLogger('telegram.bot')
 
     def start(self, arg, **payload):
-        return 'Not yet implemented'
+        return """*Поддерживаемые команды*:
+`weather` - погода в Симбирске
+`andecote` - случайные анекдоты от anekdot.ru
+`md5 [text]` - посчитать md5 от текста
+`say {@channel} {text}` - сказать от имени бота в указанный канал
+
+в привате можно слать комманды без начального слеша, например:
+`md5 test`
+Если бот не реагирует на команды в канале, попробуйте указать команду с его именем, например,
+`/md5@moonshine_bot test`
+"""
 
     def help(self, arg, **payload):
         return self.start(arg, **payload)
@@ -46,8 +56,8 @@ class CommandsProcessor(object):
 
     def weather(self, arg, **payload):
         data = xmltodict.parse(urllib.urlopen('http://rp5.ru/rss/507958/ru'))
-        return '*' + data['feed']['title']['#text'] + "*\n__" + \
-            data['feed']['entry']['title']['#text'] + "__\n" + \
+        return '*' + data['feed']['title']['#text'] + "*\n_" + \
+            data['feed']['entry']['title']['#text'] + "_\n" + \
             data['feed']['entry']['summary']['#text'] + "\n[Подробнее ...](" + \
             data['feed']['entry']['link']['@href'] + ")"
 
@@ -55,14 +65,18 @@ class CommandsProcessor(object):
         parts = arg.split(None, 1)
         if len(parts) != 2:
             return None
-        self.TelegramBot.sendMessage(parts[0], parts[1], parse_mode='Markdown')
+        try:
+            self.TelegramBot.sendMessage(parts[0], parts[1], parse_mode='Markdown')
+        except telepot.exception.TelegramError as exception:
+            chat_id = payload['message']['chat']['id']
+            self.TelegramBot.sendMessage(chat_id, exception.description, parse_mode='Markdown')
         return None
 
     def bad_request(self, arg, **payload):
         if 'message' in payload and 'chat' in payload['message'] and \
            'type' in payload['message']['chat'] and \
            payload['message']['chat']['type'] == 'private':
-            return 'Bad request'
+            return 'Bad request. Use `help` to list supported commands.'
         return None
 
     def dispatch(self, **payload):
